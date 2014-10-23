d1d610d6750da98277151543fac8840e67611aac
Turcan Vasile <vasile.turcan@wearepropeople.com>
2014-10-23 13:03:49 +0300

    Mysql default config my.cnf lines added

--- github/files/vagrant/puppet/puphpet/puppet/manifest.pp
+++ github/files/vagrant/puppet/puphpet/puppet/manifest.pp
@@ -1543,6 +1543,49 @@ include ansible::master
 file_line { 'innodb_file_per_table':
    path    => '/etc/mysql/my.cnf',
    line    => 'innodb_file_per_table = 1',
-   require => ['mysql'],
+   notify  => Service['mysql'],
+}
+
+file_line { 'tmp_table_size':
+   path    => '/etc/mysql/my.cnf',
+   line    => 'tmp_table_size = 160M',
+   notify  => Service['mysql'],
+}
+
+file_line { 'max_heap_table_size':
+   path    => '/etc/mysql/my.cnf',
+   line    => 'max_heap_table_size = 160M',
+   notify  => Service['mysql'],
+}
+
+file_line { 'character-set-server':
+   path    => '/etc/mysql/my.cnf',
+   line    => 'character-set-server=utf8',
+   notify  => Service['mysql'],
+}
+
+file_line { 'collation-server':
+   path    => '/etc/mysql/my.cnf',
+   line    => 'collation-server=utf8_unicode_ci',
+   notify  => Service['mysql'],
+}
+
+file_line { 'innodb_flush_log_at_trx_commit':
+   path    => '/etc/mysql/my.cnf',
+   line    => 'innodb_flush_log_at_trx_commit = 0 # or 2',
+   notify  => Service['mysql'],
+}
+
+file_line { 'query_cache_size':
+   path    => '/etc/mysql/my.cnf',
+   line    => 'query_cache_size = 16M',
+   notify  => Service['mysql'],
+}
+
+file_line { 'table_cache':
+   path    => '/etc/mysql/my.cnf',
+   line    => 'table_cache = 800',
+   notify  => Service['mysql'],
+}
       
-      }
+   
