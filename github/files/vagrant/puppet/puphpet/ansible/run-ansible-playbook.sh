c17270718cbafcf71273bbe4dae01d3823e3d2cb
Artyom Miroshnik <artyom.miroshnik@propeople.com.ua>
2015-03-14 16:22:43 +0200

    Move Solr installation from manifest to ansible provisioning

--- github/files/vagrant/puppet/puphpet/ansible/run-ansible-playbook.sh
+++ github/files/vagrant/puppet/puphpet/ansible/run-ansible-playbook.sh
@@ -3,3 +3,7 @@
 echo 'Install composer'
 ansible-playbook /vagrant/puphpet/ansible/composer.yml
 echo 'Finished installing composer'
+
+echo 'Install Apache Solr'
+ansible-playbook /vagrant/puphpet/ansible/solr.yml
+echo 'Finished installing Apache Sole'
