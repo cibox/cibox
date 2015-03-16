9c56ffe97457fc98b46d9bde70b72b25399246f5
Artyom Miroshnik <artyom.miroshnik@propeople.com.ua>
2015-03-16 11:09:30 +0200

    Add solr to vagrant box

--- github/files/vagrant/puppet/puphpet/ansible/run-ansible-playbook.sh
+++ github/files/vagrant/puppet/puphpet/ansible/run-ansible-playbook.sh
@@ -11,3 +11,7 @@ echo 'Finished installing php'
 echo 'Install composer'
 ansible-playbook /vagrant/puphpet/ansible/composer.yml
 echo 'Finished installing composer'
+
+echo 'Install Apache Solr'
+ansible-playbook /vagrant/puphpet/ansible/solr.yml
+echo 'Finished installing Apache Solr'
