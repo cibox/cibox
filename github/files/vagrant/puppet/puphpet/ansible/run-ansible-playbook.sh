afbb8c9edf97a0e32e2237f1a4a2cc77fc9e099f
Alexander Schedrov <alexander.schedrov@gmail.com>
2015-03-25 12:23:35 +0200

    Temporary fix for sendmail

--- github/files/vagrant/puppet/puphpet/ansible/run-ansible-playbook.sh
+++ github/files/vagrant/puppet/puphpet/ansible/run-ansible-playbook.sh
@@ -11,6 +11,7 @@ playbooks=(
 /vagrant/puphpet/ansible/composer.yml \
 /vagrant/puphpet/ansible/drush.yml \
 /vagrant/puphpet/ansible/solr.yml \
+/vagrant/puphpet/ansible/sendmail.yml \
 )
 
 for i in "${playbooks[@]}"
