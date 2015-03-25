68f416fe547e5d0219d3da5d6fefec0d42c10ca6
Alexander Schedrov <alexander.schedrov@gmail.com>
2015-03-25 12:31:33 +0200

    Reorder playbooks

--- github/files/vagrant/puppet/puphpet/ansible/run-ansible-playbook.sh
+++ github/files/vagrant/puppet/puphpet/ansible/run-ansible-playbook.sh
@@ -3,6 +3,7 @@
 export PYTHONUNBUFFERED=1
 
 playbooks=(
+/vagrant/puphpet/ansible/sendmail.yml \
 /vagrant/puphpet/ansible/apache.yml \
 /vagrant/puphpet/ansible/php.yml \
 /vagrant/puphpet/ansible/php-xdebug.yml \
@@ -11,7 +12,6 @@ playbooks=(
 /vagrant/puphpet/ansible/composer.yml \
 /vagrant/puphpet/ansible/drush.yml \
 /vagrant/puphpet/ansible/solr.yml \
-/vagrant/puphpet/ansible/sendmail.yml \
 )
 
 for i in "${playbooks[@]}"
