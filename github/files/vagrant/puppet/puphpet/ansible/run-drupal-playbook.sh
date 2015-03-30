d1b2ef10415fbf33c2f2c9fcea48d85637d2f3f4
podarok <podarok@ua.fm>
2015-03-30 16:36:36 +0300

    Heh, we should use old grammar_parser

--- github/files/vagrant/puppet/puphpet/ansible/run-drupal-playbook.sh
+++ github/files/vagrant/puppet/puphpet/ansible/run-drupal-playbook.sh
@@ -11,7 +11,7 @@ playbooks=(
 for i in "${playbooks[@]}"
 do
    echo "Install "${i}
-   ansible-playbook ${i}
+   ansible-playbook -vvvv ${i}
    if [ "$?" == "0" ]; then
     echo "Finished installing "${i}
    else
