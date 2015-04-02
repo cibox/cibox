734d360c9e29e745b89471c8cb1d718848b31b7c
podarok <podarok@ua.fm>
2015-04-02 17:55:24 +0300

    Reverting solr back for merging before refactoring to roles

--- github/files/vagrant/puppet/puphpet/ansible/run-ansible-playbook.sh
+++ github/files/vagrant/puppet/puphpet/ansible/run-ansible-playbook.sh
@@ -11,7 +11,7 @@ playbooks=(
 /vagrant/puphpet/ansible/memcached.yml \
 /vagrant/puphpet/ansible/composer.yml \
 /vagrant/puphpet/ansible/drush.yml \
-#/vagrant/puphpet/ansible/solr.yml \
+/vagrant/puphpet/ansible/solr.yml \
 )
 
 for i in "${playbooks[@]}"
