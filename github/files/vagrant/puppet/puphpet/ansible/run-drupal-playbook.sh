0fa9cda34857fb62b8f6102af3e91367812227f5
podarok <podarok@ua.fm>
2015-04-02 13:18:43 +0300

    Preparing to devops.yml

--- github/files/vagrant/puppet/puphpet/ansible/run-drupal-playbook.sh
+++ github/files/vagrant/puppet/puphpet/ansible/run-drupal-playbook.sh
@@ -4,7 +4,7 @@ export PYTHONUNBUFFERED=1
 
 playbooks=(
 /var/www/drupal/reinstall.yml \
-/var/www/drupal/security.yml \
+/var/www/drupal/devops.yml \
 /var/www/drupal/sniffers.yml \
 /var/www/drupal/tests.yml \
 )
