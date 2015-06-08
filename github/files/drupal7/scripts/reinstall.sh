c268165b81b354d9cba74880f8fb7da78a3bf361
Artyom Miroshnik <artyom.miroshnik@propeople.com.ua>
2015-06-08 15:26:30 +0300

    Optimize reinstall bash script

--- github/files/drupal7/scripts/reinstall.sh
+++ github/files/drupal7/scripts/reinstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PYTHONUNBUFFERED=1
 EXTRA_VARS=''
-if [ -z '$1' ] && [ $1 == *"—-windows"* ]; then
-  EXTRA_VARS='—extra-vars=“is-windows=true”'
+if [ ! -z '$1' -a $1 = "--windows" ]; then
+  EXTRA_VARS='--extra-vars=“is-windows=true”'
 fi
 time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local ${EXTRA_VARS}
