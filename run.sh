b53dc2cac2e9516a5c01c6f2395dad61cace8efb
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-01-25 15:37:08 +0200

    Runner with extender grep pattern

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 export PYTHONUNBUFFERED=1
 
 # Get server IP from config.yml
-cat config.yml | grep -e '^server_ip:\s.*$' | cut -d" " -f2
+IP=`cat config.yml | grep -E '^server_ip:\s.*$' | cut -d" " -f2`
 
 ansible-playbook -vvvv -i $IP, services/jenkinsbox.yml --extra-vars "@config.yml"
