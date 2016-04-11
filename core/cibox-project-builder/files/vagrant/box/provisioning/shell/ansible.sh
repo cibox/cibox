c00b2ac9259f2116d422bd17930e0fd6ceb2e37f
Andrii Podanenko <podarokua@gmail.com>
2016-04-11 16:55:44 +0300

    Update ansible.sh

--- core/cibox-project-builder/files/vagrant/box/provisioning/shell/ansible.sh
+++ core/cibox-project-builder/files/vagrant/box/provisioning/shell/ansible.sh
@@ -16,7 +16,7 @@ echo 'Finished installing base packages for ansible'
 echo 'Installing ansible'
 
 echo "Installing pip via easy_install."
-sudo apt-get install python-pip python-dev build-essential
+sudo apt-get -y --force-yes install python-pip python-dev build-essential
 sudo pip install --upgrade pip 
 sudo pip install --upgrade virtualenv 
 sudo pip install --upgrade pip
