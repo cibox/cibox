eeab32c00ead7103a565bd01a11ce6ac5a3119fa
Andrii Podanenko <podarokua@gmail.com>
2016-08-22 11:02:58 +0300

    Update run-ansible-playbook.sh

--- core/cibox-project-builder/files/vagrant/box/provisioning/ansible/run-ansible-playbook.sh
+++ core/cibox-project-builder/files/vagrant/box/provisioning/ansible/run-ansible-playbook.sh
@@ -5,16 +5,16 @@ export PYTHONUNBUFFERED=1
 # Commented playbook stay here just in case you'll need it for updating an image or
 # specific software settings.
 playbooks=(
-# /vagrant/provisioning/ansible/playbooks/sendmail.yml \
+/vagrant/provisioning/ansible/playbooks/sendmail.yml \
 /vagrant/provisioning/ansible/playbooks/misc.yml \
 /vagrant/provisioning/ansible/playbooks/www.yml \
-# /vagrant/provisioning/ansible/playbooks/php.yml \
-# /vagrant/provisioning/ansible/playbooks/php-xdebug.yml \
-# /vagrant/provisioning/ansible/playbooks/mysql.yml \
-# /vagrant/provisioning/ansible/playbooks/memcached.yml \
+/vagrant/provisioning/ansible/playbooks/php.yml \
+/vagrant/provisioning/ansible/playbooks/php-xdebug.yml \
+/vagrant/provisioning/ansible/playbooks/mysql.yml \
+/vagrant/provisioning/ansible/playbooks/memcached.yml \
 /vagrant/provisioning/ansible/playbooks/composer.yml \
 /vagrant/provisioning/ansible/playbooks/drush.yml \
-# /vagrant/provisioning/ansible/playbooks/solr.yml \
+/vagrant/provisioning/ansible/playbooks/solr.yml \
 /vagrant/provisioning/ansible/playbooks/sniffers.yml \
 /vagrant/provisioning/ansible/playbooks/docker.yml \
 )
@@ -22,7 +22,7 @@ playbooks=(
 for i in "${playbooks[@]}"
 do
    echo "Install "${i}
-   ansible-playbook -i 'localhost,' ${i} --connection=local
+   ansible-playbook -vvvv -i 'localhost,' ${i} --connection=local
    if [ "$?" == "0" ]; then
     echo "Finished installing "${i}
    else
