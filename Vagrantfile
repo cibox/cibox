b2c347e2b975058625003f78f9182d592e680844
valthebald <valery@wisetrout.com>
2016-10-19 19:42:10 +0300

    Issue #518: Fix example build files

--- Vagrantfile
+++ Vagrantfile
@@ -5,5 +5,5 @@
 
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/xenial64"
-  config.vm.provision "shell", path: "builder.sh"
+  config.vm.provision "shell", path: "builder-setup.sh"
 end
--- builder-setup.sh
+++ builder-setup.sh
@@ -4,10 +4,11 @@
 # Copy pre-generated keys.
 cp /vagrant/ssh-keys/* /home/ubuntu/.ssh/
 sudo apt-get -y install software-properties-common
-# Uncomment the following 2 lines when using Ubuntu 14.04, to get python-2.7.9
+# Uncomment the following 3 lines when using Ubuntu 14.04, to get python-2.7.9
 #wget https://gist.githubusercontent.com/drjerry/06041e0d78935e9d0b9e/raw/5af1922a92e2cd9eb2fb4e620191788ea2c66b67/build-python-2.7.9.sh
 #sudo sh build-python-2.7.9.sh
-sudo apt-add-repository ppa:ansible/ansible-1.9
+# sudo apt-add-repository ppa:ansible/ansible-1.9
 sudo apt-get -y update
-sudo apt-get -y install ansible python-pip
+sudo apt-get install -y ansible python-pip
 sudo pip install jinja2
+sudo pip install ansible==1.9.4
