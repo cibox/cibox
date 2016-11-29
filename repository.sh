ba545c97e47d5c7b87303a7ceef47d7bdbc0acb5
Pavel Loparev <pavel.loparev@ffwagency.com>
2016-11-29 22:25:15 +0200

    Requirements in a separate file

--- repository.sh
+++ repository.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 
-# Determine whether ansible is installed or not.
-info="Please install ansible 1.9.*. Visit http://docs.cibox.tools/en/latest/Requirements for more information."
-is_ansible_installed=false
-ansible --version >/dev/null 2>&1 && { is_ansible_installed=true; }
-if [ "$is_ansible_installed" = false ]; then
-  echo ${info}
-  exit 1
-fi
-
-# If ansible is installed but version isn't 1.9.* then interrupt script execution.
-is_ansible_1_9=$(ansible --version | grep "1.9")
-if [ -z "$is_ansible_1_9" ]; then
-  echo "Current ansible version is not supported by CIBOX.\n"${info}
-  exit 1
-fi
+. ./requirements.sh
 
 export PYTHONUNBUFFERED=1
 ansible-playbook -i 127.0.0.1, services/repository.yml
