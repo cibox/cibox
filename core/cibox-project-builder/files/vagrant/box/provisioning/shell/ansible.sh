926c6d459ddcd51088a7d6c3e3770db4818aaecf
Andrii Podanenko <podarokua@gmail.com>
2020-11-13 13:41:45 +0200

    change sudo to become

--- core/cibox-project-builder/files/vagrant/box/provisioning/shell/ansible.sh
+++ core/cibox-project-builder/files/vagrant/box/provisioning/shell/ansible.sh
@@ -7,7 +7,7 @@ export PYTHONUNBUFFERED=1
 export ANSIBLE_FORCE_COLOR=true
 # ansible needs python version 2.7.9 to avoid SNIMissingWarning.
 # see http://urllib3.readthedocs.io/en/latest/security.html#snimissingwarning
-apt-get -y --force-yes install python 2.7.9
+apt-get -y --force-yes install python
 apt-get -y --force-yes install python-dev python-simplejson sudo curl make rsync git libmysqlclient-dev apparmor-utils >/dev/null
 
 # because basic ubuntu is too stripped down we need to add logging.
@@ -21,21 +21,22 @@ echo 'Finished installing base packages for ansible'
 echo 'Installing ansible'
 
 echo "Installing pip via easy_install."
-sudo apt-get -y --force-yes install unzip python-pip python-dev build-essential libffi-dev libssl-dev
+sudo apt-get -y --force-yes install unzip python3-pip python-dev build-essential libffi-dev libssl-dev
 
-sudo PYTHONWARNINGS=ignore pip install --upgrade pip
-sudo PYTHONWARNINGS=ignore pip install --upgrade virtualenv
-sudo PYTHONWARNINGS=ignore pip install --upgrade pip
-sudo PYTHONWARNINGS=ignore pip install --upgrade cffi
-sudo PYTHONWARNINGS=ignore pip install --upgrade cryptography
-sudo PYTHONWARNINGS=ignore pip install --upgrade Jinja2
+sudo PYTHONWARNINGS=ignore pip3 install --upgrade pip
+sudo PYTHONWARNINGS=ignore pip3 install --upgrade virtualenv
+sudo PYTHONWARNINGS=ignore pip3 install --upgrade pip
+sudo PYTHONWARNINGS=ignore pip3 install --upgrade cffi
+sudo PYTHONWARNINGS=ignore pip3 install --upgrade cryptography
+sudo PYTHONWARNINGS=ignore pip3 install --upgrade Jinja2
 # Make sure setuptools are installed correctly.
-pip install setuptools --no-use-wheel --upgrade
+sudo apt install -y python3-testresources
+pip3 install setuptools --upgrade
 
 echo "Installing required python modules."
-pip install paramiko pyyaml jinja2 markupsafe MySQL-python
+pip3 install paramiko pyyaml jinja2 markupsafe MySQL-python
 
 echo "Installing Ansible."
-pip install ansible==1.9.6
+pip3 install ansible
 
 echo 'Finished installing ansible'
